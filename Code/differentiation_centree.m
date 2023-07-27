3810deff2c49f96ba140991752e0fe365e994e8b
Marc-Olivier Fecteau <marc-olivier.fecteau@usherbrooke.ca>
2023-07-27 12:40:22 -0400

    void commit

---
+++ Code/differentiation_centree.m
@@ -0,0 +1,11 @@
+% Différentiation numérique centrée
+
+function df = differentiation_centree(x, f)
+	df = zeros(1, length(f));
+	df(1) = (f(2) - f(1)) / (x(2) - x(1));
+	for i = 2:(length(x)-1)
+		df(i) = (f(i+1) - f(i-1)) / (x(i+1) - x(i-1));
+    end
+	df(end) = (f(end) - f(end-1)) / (x(end) - x(end-1));
+end
+
